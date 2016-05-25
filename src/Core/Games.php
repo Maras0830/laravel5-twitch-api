3eaee2f587f90c4cca9abfa542f337d54cdd93ff
Maras Chen <Maras0830@gmail.com>
2016-05-26 01:41:26 +0800

    Update Games.php to support options.

--- src/Core/Games.php
+++ src/Core/Games.php
@@ -1,1 +1,33 @@
-<?phpnamespace Maras0830\TwitchApi\Core;/** * Class Games * @package Maras0830\TwitchApi\Core */class Games extends Base{    /**     * Get games by number of viewers.     *     * @return mixed     */    public function topGames()    {        $response = $this->client->get('/kraken/games/top');        return $response->json();    }}
+<?php
+namespace Maras0830\TwitchApi\Core;
+
+/**
+ * Class Games
+ * @package Maras0830\TwitchApi\Core
+ */
+class Games extends Base
+{
+
+    /**
+     * Get games by number of viewers.
+     *
+     * @return mixed
+     */
+    public function topGames()
+    {
+        $availableOptions = ['limit', 'offset'];
+        
+        $query = [];
+
+        foreach ($availableOptions as $option) {
+            if (isset($options[ $option ])) {
+                $query[ $option ] = $options[ $option ];
+            }
+        }
+        
+        $parameters[ 'query' ] = $query;
+        $response = $this->client->get('/kraken/games/top', $parameters);
+
+        return $response->json();
+    }
+}
